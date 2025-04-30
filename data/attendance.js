d4f762be9fd3476506d95100ccc3b5bdd2fc8ef8
ArchiitRajanala <146397638+ArchiitRajanala@users.noreply.github.com>
2025-04-30 03:15:21 -0400

    more edits to attendance
    edits to attendance and index and lectures, used same validation as other methods

--- data/attendance.js
+++ data/attendance.js
@@ -12,9 +12,37 @@ import { stringValidate } from "../validation.js";
 //     "createdAt": "timestamp"  
 // }
 
-let createAttendance = async (lectureId, courseId, studentId, status, points) => {
+// uses createAttendance, updateAttendance, it will be adding 1 point for each class missed, for now excused does not add points
+let createAttendance = async (lectureId, courseId, studentId, status) => {
 
     const attendanceCollection = await attendance();
+    lectureId = stringValidate(lectureId);
+    courseId = stringValidate(courseId);
+    studentId = stringValidate(studentId);
+    status = stringValidate(status);
+    if (!ObjectId.isValid(lectureId)) {
+        throw "Invalid lecture ID.";
+    }
+    if (!ObjectId.isValid(courseId)) {
+        throw "Invalid course ID.";
+    }
+    if (!ObjectId.isValid(studentId)) {
+        throw "Invalid student ID.";
+    }
+    if (status === "present" || status === "absent" || status === "excused") {
+        throw "Status must be either 'present' or 'absent or excused '.";
+    }
+    if (status === "present") {
+        points = 0; 
+    }
+    else if (status === "excused") {
+        points = 0
+    }
+    else {
+        point = 1
+    }
+
+
 
     attendanceCollection.insertOne({
         lectureId: lectureId,
@@ -35,11 +63,37 @@ let updateAttendance = async (attendanceId, updates) => {
     if (!attendanceRecord) {
         throw "Attendance not found";
     }
+    let existingAttendance = await attendancCollection.findOne({ _id: new ObjectId(attendanceId) });
+    if (!existingAttendance) {
+        throw "Attendance record not found";
+    }
+
+    if (updates.status) {
+        updates.status = stringValidate(updates.status);
+        if (updates.status !== "present" && updates.status !== "absent" && updates.status !== "excused") {
+            throw "Status must be either 'present', 'absent', or 'excused'.";
+        }
+    }
+    if (updates.points) {
+        updates.points = parseInt(updates.points);
+        if (isNaN(updates.points) || updates.points < 0) {
+            throw "Points must be a non-negative number.";
+        }
+    }
+    updates.updatedAt = new Date();
+    const updateInfo = await attendancCollection.updateOne(
+        { _id: new ObjectId(attendanceId) },
+        { $set: updates }
+    );
+    if (updateInfo.modifiedCount === 0) {
+        throw "Could not update attendance record";
+    }
+    return { isUpdated: true, attendanceId: attendanceId };
 
 
 }
 
 export default {
-    createAttendance
-
+    createAttendance,
+    updateAttendance
 }
