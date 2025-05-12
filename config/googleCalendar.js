44336cf498cc15d753a8cd20a372ccb49e5fa4b3
Anik <asdoshi@hotmail.com>
2025-05-11 23:37:19 -0400

    update smart cal functionality and removed exposed keys from commit

--- config/googleCalendar.js
+++ config/googleCalendar.js
@@ -1,13 +1,28 @@
 import { google } from 'googleapis';
-import { readFileSync } from 'fs';
+import fetch from 'node-fetch'; // make sure to install this
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+const KEY_URL = 'https://gist.githubusercontent.com/anikdoshi2003/9f3368554f8797e30a149e2838d0c7db/raw/scholario-459602-2996118ff3d3.json'; // use raw URL
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: 'internal-private-files/scholario-459602-2996118ff3d3.json', // downloaded key
-  scopes: SCOPES,
-});
+async function getCalendarInstance() {
+  const response = await fetch(KEY_URL);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch key file: ${response.statusText}`);
+  }
 
-const calendar = google.calendar({ version: 'v3', auth });
+  const credentials = await response.json();
 
-export default calendar;
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: SCOPES,
+  });
+
+  const calendar = google.calendar({ version: 'v3', auth });
+
+  return calendar;
+}
+
+export default await getCalendarInstance();
