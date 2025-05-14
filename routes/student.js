c54904bf0c46081e4b5de3244ef1e3c16b372154
Anik <asdoshi@hotmail.com>
2025-05-13 20:05:50 -0400

    fixed contact-tas

--- routes/student.js
+++ routes/student.js
@@ -1452,7 +1452,7 @@ router.route("/contact-tas").get(async (req, res) => {
 
     res.render("student/student", {
       layout: "main",
-      partialToRender: "contactTAs",
+      partialToRender: "contactTas",
       user: withUser(req),
       currentPage: "contactTAs",
       courses: coursesWithTAs,
